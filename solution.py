c6e8ae7828a3b02ae48a887a8737df15b96bfeff
Aaronmartino23 <85592040+Aaronmartino23@users.noreply.github.com>
2022-09-09 22:01:01 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,8 @@
-
 ### welcome_assignment_answers
 ### Input - All eight questions given in the assignment.
 ### Output - The right answer for the specific question.
+
+
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
@@ -20,9 +21,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = int("5")
+        answer = int(5)
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = int("4")
+        answer = int(4)
     return(answer)
 # Complete all the questions.
 # Complete all the questions.
@@ -32,8 +33,6 @@ if __name__ == "__main__":
     #use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
-
-
 
 #Questions:
 #"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
